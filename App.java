f605321f0324ace7f64a33a41c9b84d906f2a9aa
veera-ops <106005430+veera-ops@users.noreply.github.com>
2025-06-25 11:57:10 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java ECR Demo!");
+        System.out.println("Hello from Java application ECR Demo!");
     }
 }
